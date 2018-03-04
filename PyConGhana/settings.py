2d5967ed04d88b073d4828d7ac0cf83987f2098e
Williano <paawilly17@gmial.com>
2018-03-03 23:44:19 -1200

    Update on Settings

--- PyConGhana/settings.py
+++ PyConGhana/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from .secrets import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '0hl14(&0l)7=rcft0!!op7+7^1((d!^j$lnu6b9cyh30_$l71g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -35,6 +35,8 @@ GRAPPELLI_ADMIN_TITLE = "PYCON GHANA"
 # Application definition
 
 INSTALLED_APPS = [
+    'jet.dashboard',   
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,9 +47,7 @@ INSTALLED_APPS = [
 # Required apps
     'crispy_forms',
     'rest_framework',
-    'jet',
-    'jet.dashboard',
-#    'markitup',
+    #'markitup',
 
 # my apps
     'home',
