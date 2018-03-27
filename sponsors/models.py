cd5b82b8dfdaf1dd885c7800ea54d1e621322adb
mawy7 <mannieyoung7@gmail.com>
2018-03-27 23:38:07 +0000

    updates 3

--- sponsors/models.py
+++ sponsors/models.py
@@ -16,7 +16,7 @@ class Sponsor(models.Model):
     category = models.CharField(max_length=15, choices=SPONSOR_PACKAGES)
     logo = models.ImageField(max_length=255, blank=True, null=True)
     type = models.CharField("sponsor type", max_length=1, choices=SPONSOR_TYPE)
-    website = models.URLField("website URL/ Twitter handle", max_length=200, blank=True, null=True)
+    website = models.URLField(default='',help_text="website URL/ Twitter handle", max_length=200, blank=True, null=True)
 
     class Meta:
         managed = True
